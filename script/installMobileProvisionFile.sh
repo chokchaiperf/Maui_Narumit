49ee211754c49a4a89f33472e60d398f065e2d4e
chokchai <chokchai9900@gmai.com>
2024-11-26 00:27:18 +0700

    implement for test local runner

---
+++ script/installMobileProvisionFile.sh
@@ -0,0 +1,17 @@
+if [ ! $# == 1 ]; then
+echo "Usage: $0 (path/to/mobileprovision)"
+exit
+fi
+
+mp=$1
+
+uuid=`./mpParse -f ${mp} -o uuid`
+
+echo "Found UUID $uuid"
+
+output="~/Library/MobileDevice/Provisioning Profiles/$uuid.mobileprovision"
+
+echo "copying to $output.."
+cp "${mp}" "$output"
+
+echo "done"
